ac01c0baba0c917dea4f637a992135aefc316b1c
sadam <sadamnagoor@example.com>
2024-01-07 13:43:32 +0530

    lazy loading vs eager loading

--- src/main/java/com/easybytes/beans/Person.java
+++ src/main/java/com/easybytes/beans/Person.java
@@ -2,9 +2,12 @@ package com.easybytes.beans;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
 @Data
 @Component(value = "personBean")
+@Lazy
 public class Person {
 
     private String name;
@@ -12,7 +15,8 @@ public class Person {
     private final Vehicle vehicle;
 
     @Autowired
-    public Person(Vehicle vehicle){
+    public Person(Vehicle vehicle) {
+        System.out.println("vehicle object created");
         this.vehicle = vehicle;
     }
 }
